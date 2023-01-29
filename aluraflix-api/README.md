70cfa11808eecf37bacfb2f4b0203ffbc1ac4b21
DENNY GABRIEL BARBOSA DE MORAIS <102764595+dennyGabrielBarbosa@users.noreply.github.com>
2023-01-29 14:51:59 -0300

    Delete aluraflix-api directory

--- aluraflix-api/README.md
+++
@@ -1,3 +0,0 @@
-<h1>AluraFlix API</h1>
-
-> Status do projeto: Em desenvolvimento
--- aluraflix-api/java/com/challenge/videos/repository/CategoriasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.videos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.challenge.videos.modelo.Categorias;
-
-public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
-	
-}
